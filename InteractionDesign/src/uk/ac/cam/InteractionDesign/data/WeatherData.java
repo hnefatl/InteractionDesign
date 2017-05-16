c7a9f427f4e5ea853604c3fd63d1dfbee37c8d1e
Keith <kc506@cam.ac.uk>
2017-05-16 14:05:47 +0100

    Added skeleton data API

---
+++ InteractionDesign/src/uk/ac/cam/InteractionDesign/data/WeatherData.java
@@ -0,0 +1,42 @@
+package uk.ac.cam.InteractionDesign.data;
+
+import net.aksingh.owmjapis.*;
+
+public class WeatherData
+{
+	private boolean hasTemp;
+	private float temp;
+	
+	private boolean hasTempRange;
+	private float tempMin;
+	private float tempMax;
+	
+	private boolean hasRain;
+	private float rain;
+	
+	public WeatherData(CurrentWeather weather)
+	{
+		hasTemp = weather.hasMainInstance() && weather.getMainInstance().hasTemperature();
+		hasTempRange = 	weather.hasMainInstance() &&
+						weather.getMainInstance().hasMinTemperature() &&
+						weather.getMainInstance().hasMaxTemperature(); 
+		
+		if (hasTemp)
+			temp = weather.getMainInstance().getTemperature();
+			
+		if (hasTempRange)
+		{
+			tempMin = weather.getMainInstance().getMinTemperature();
+			tempMax = weather.getMainInstance().getMaxTemperature();
+		}
+		
+		if (hasRain)
+			rain = weather.getRainInstance().getRain();
+
+	}
+	
+	public static WeatherData getWeather()
+	{
+		OpenWeatherMap owm = new OpenWeatherMap("");
+	}
+}
