1b4bea638df18a833e613cd34d29152d79e0cd33
Keith <kc506@cam.ac.uk>
2017-05-20 22:01:26 +0100

    Added night WeatherType support

--- InteractionDesign/src/com/github/hnefatl/interactiondesign/data/WeatherType.java
+++ InteractionDesign/src/com/github/hnefatl/interactiondesign/data/WeatherType.java
@@ -2,40 +2,35 @@ package com.github.hnefatl.interactiondesign.data;
 
 public enum WeatherType
 {
-	CLEAR,
+	CLEAR_DAY,
+	CLEAR_NIGHT,
 	RAIN,
 	SNOW,
 	SLEET,
 	WIND,
 	FOG,
 	CLOUDY,
-	PARTLY_CLOUDY,
+	PARTLY_CLOUDY_DAY,
+	PARTLY_CLOUDY_NIGHT,
 	UNKNOWN;
 	
 	public static WeatherType parse(String text)
 	{
-		// Options in version 0.10 of the darksy API are:
+		// Options in version 0.10 of the darksky API are:
 		// clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
 		
 		switch (text)
 		{
-		case "clear-day":
-		case "clear-night": 		return CLEAR;
-		
+		case "clear-day":			return CLEAR_DAY;
+		case "clear-night": 		return CLEAR_NIGHT;
 		case "rain":				return RAIN;
-		
 		case "snow":				return SNOW;
-		
 		case "sleet":				return SLEET;
-		
 		case "wind":				return WIND;
-		
 		case "fog":					return FOG;
-		
 		case "cloudy":				return CLOUDY;
-		
-		case "partly-cloudy-day":
-		case "partly-cloudy-night":	return PARTLY_CLOUDY;
+		case "partly-cloudy-day":	return PARTLY_CLOUDY_DAY;
+		case "partly-cloudy-night":	return PARTLY_CLOUDY_NIGHT;
 		}
 		
 		return UNKNOWN;
