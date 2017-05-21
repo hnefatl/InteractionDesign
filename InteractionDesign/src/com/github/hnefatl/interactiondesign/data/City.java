4436080737ef89c783c924131f4c9c22f0ba8c98
Keith <kc506@cam.ac.uk>
2017-05-22 00:36:18 +0100

    Adding place name -> lat/long support. Not functional quite yet, but general interface structure should remain the same from now on

--- InteractionDesign/src/com/github/hnefatl/interactiondesign/data/City.java
+++ InteractionDesign/src/com/github/hnefatl/interactiondesign/data/City.java
@@ -1,6 +1,8 @@
 package com.github.hnefatl.interactiondesign.data;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
@@ -15,6 +17,9 @@ public class City
 {
 	// API key for the Google Geocoding API (registered under Keith's account)
 	private static final String GOOGLE_API_KEY = "AIzaSyAYSQffkovQev2FudqSKLVkhelDmycilkw";
+	
+	// Need to keep only one reference alive (API recommends)
+	private static GeoApiContext context;
 	
 	private String cityName;
 	private double lat;
@@ -35,32 +40,19 @@ public class City
 	public static City getCity(double lat, double lon) throws DataNotFoundException, InvalidFormatException, CityNotFoundException
 	{
 		try
-		{
-			GeoApiContext context = new GeoApiContext();
-			context.setApiKey(GOOGLE_API_KEY);
-			
-			GeocodingResult[] results = GeocodingApi.reverseGeocode(context, new LatLng(lat, lon)).await();
+		{			
+			GeocodingResult[] results = GeocodingApi.reverseGeocode(getContext(), new LatLng(lat, lon)).await();
 			
 			// Look at each result, pull out the locality
 			for (GeocodingResult r : results)
 			{
-				if (contains(r.types, AddressType.LOCALITY))
+				try
 				{
-					String country = null;
-					String city = null;
+					City c = parse(r);
 					
-					// Extract the town and country from the result
-					for (AddressComponent a : r.addressComponents)
-					{
-						if (contains(a.types, AddressComponentType.LOCALITY))
-							city = a.shortName;
-						if (contains(a.types, AddressComponentType.COUNTRY))
-							country = a.longName;
-					}
-					
-					if (city != null && country != null)
-						return new City(city, lat, lon, country);
+					return c;
 				}
+				catch (CityNotFoundException e) { } // Ignore, we have other results to try
 			}
 			
 			// City couldn't be found
@@ -75,6 +67,61 @@ public class City
 			throw new InvalidFormatException(e);
 		}
 	}
+	
+	/**
+	 * Returns the list of cities matching the specified query
+	 */
+	public static List<City> getCities(String locationName) throws DataNotFoundException, InvalidFormatException
+	{	
+		try
+		{
+			List<City> cities = new ArrayList<>();
+			
+			GeocodingResult[] results = GeocodingApi.geocode(getContext(), locationName).await();
+			for (GeocodingResult r : results)
+			{
+				try
+				{
+					City c = parse(r);
+					cities.add(c);
+				}
+				catch (CityNotFoundException e) { } // Ignore, there's more cities to try
+			}
+			
+			return cities;
+		}
+		catch (IOException|InterruptedException e) // Network error
+		{
+			throw new DataNotFoundException(e);
+		}
+		catch (ApiException e) // Returned data was invalid
+		{
+			throw new InvalidFormatException(e);
+		} 
+	}
+	
+	private static City parse(GeocodingResult data) throws CityNotFoundException
+	{
+		if (contains(data.types, AddressType.LOCALITY))
+		{
+			String country = null;
+			String city = null;
+			
+			// Extract the town and country from the result
+			for (AddressComponent a : data.addressComponents)
+			{
+				if (contains(a.types, AddressComponentType.LOCALITY))
+					city = a.shortName;
+				if (contains(a.types, AddressComponentType.COUNTRY))
+					country = a.longName;
+			}
+			
+			if (city != null && country != null)
+				return new City(city, data.geometry.location.lat, data.geometry.location.lng, country);
+		}
+		
+		throw new CityNotFoundException();
+	}
 	private static <T> boolean contains(T[] items, T value)
 	{
 		for (T i : items)
@@ -83,6 +130,13 @@ public class City
 				return true;
 		}
 		return false;
+	}
+	
+	private static GeoApiContext getContext()
+	{
+		if (context == null)
+			context = new GeoApiContext().setApiKey(GOOGLE_API_KEY);
+		return context;
 	}
 	
 	/**
@@ -112,5 +166,12 @@ public class City
 	public String getCountryName()
 	{
 		return countryName;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return 	getCityName() + ", " + getCountryName() + "\n" +
+				"Lat: " + getLat() + "\tLon: " + getLon();
 	}
 }
