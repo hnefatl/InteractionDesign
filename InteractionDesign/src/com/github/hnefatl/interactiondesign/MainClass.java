33d67f3f502d484b77768ebf62b3e5618dc505fe
EshaDasgupta <ed487@cam.ac.uk>
2017-05-23 14:18:34 +0100

    Removed some files

--- InteractionDesign/src/com/github/hnefatl/interactiondesign/MainClass.java
+++ InteractionDesign/src/com/github/hnefatl/interactiondesign/MainClass.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.util.List;
+import java.awt.List;
 import java.awt.Panel;
 import java.awt.ScrollPane;
 import java.awt.TextField;
@@ -17,6 +17,8 @@ import javax.swing.JFrame;
 import org.geonames.*;
 import com.github.hnefatl.interactiondesign.GeoLookUp;
 import com.github.hnefatl.interactiondesign.data.City;
+import com.github.hnefatl.interactiondesign.data.DataNotFoundException;
+import com.github.hnefatl.interactiondesign.data.InvalidFormatException;
 
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
@@ -24,10 +26,8 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 public class MainClass extends JFrame
 {
 	EmbeddedProc mEmbed;
-	Button mAButton;
-	Boolean MarkerBool= false;
 	JFrame MainFrame;
-	public MainClass()
+	public MainClass() throws DataNotFoundException, InvalidFormatException
 	{
 		super("Embedded Test");
 		MainFrame= new JFrame();
@@ -37,71 +37,81 @@ public class MainClass extends JFrame
 		mEmbed = new EmbeddedProc();
 		MainFrame.add(mEmbed, BorderLayout.CENTER);
 		mEmbed.init();
-		
-		//mAButton = new Button("A Button");
-		//mAButton = new Button("A Button");
 		MainFrame.add(createControlPanel(),BorderLayout.SOUTH);
 		MainFrame.add(createInfoPanel(""), BorderLayout.EAST);
+		createInfoPanel("");
 		MainFrame.setVisible(true);
 		}
 		
 	
-	public Component createInfoPanel(String place) {
-		Panel info =  new Panel(new GridLayout(2,1));	
+	public Component createInfoPanel(String place) throws DataNotFoundException, InvalidFormatException {
+		//CREATING THE PANEL WITH THE LIST OF NAMES FROM THE USER INPUT+ ANY OTHER PANELS ON THE RIGHT
+		Panel info =  new Panel();	
 	   info.add(createCityPanel(place));
-	   MainFrame.add(info, BorderLayout.EAST);
+	   if(place!=""){MainFrame.repaint();}
 	    return info;
 	}
 
-	public Component createCityPanel(String place){
+	public Component createCityPanel(String place) throws DataNotFoundException, InvalidFormatException{
+		//THE ACTUAL CITY LIST PANEL FROM THE USER INPUT
 		Panel locations = new Panel();
-		if (place==""){return locations;}
-		 //GeoLookUp g= new City(place); 		 
-		 //HashMap <String,Float[]> LocationList= g.getLoc();
-		
-		List<City> nlist= City.getCities(place);
-
-		    //String [] namelist= new String [LocationList.size()];
-		    //List nlist= new List<>();
-		    //int count=0;
-		   // for (String p:LocationList.keySet()){
-		   // 	nlist.add(p);
-		    	//count++;
-		   // }
-		    //System.out.println(count);
-		    
-		    		    
-		    //nlist.addActionListener((e)-> {String w = nlist.getSelectedItem(); 
-		    //Float[] cords=LocationList.get(w); mEmbed.placeMarker(cords);}); 
-		    
-		    ScrollPane scrollpane = new ScrollPane();
-		   scrollpane.add(nlist,ScrollPane.SCROLLBARS_AS_NEEDED);
+		ScrollPane scrollpane = new ScrollPane();
+		   
 		   scrollpane.setSize(200,650);
 		    locations.setSize(250,700);
 		    locations.add(scrollpane);
 		    System.out.println("HERE");
-		    return locations; 
+		if (place==""){return locations;}
+		
+		//SHOULD TAKE IN INPUT AND CREATE AWT LIST AND DISPLAY BUT DOESN'T :(
+		List nlist= new List();
+	    //int count=0;
+	    for (City c: City.getCities(place)){
+	    	nlist.add(c.getCityName());
+	    	//count++;
+	    }
+		scrollpane.add(nlist,ScrollPane.SCROLLBARS_AS_NEEDED); 
+		
+	    return locations; 
+		    
+	    // SHOULD TAKE SELECTED NAME FROM LIST AND PLACE MARKER ON IT 
+		    //nlist.addActionListener((e)-> {String w = nlist.getSelectedItem(); 
+		    //Float[] cords={City.}; mEmbed.placeMarker(cords);}); 
+		   
 	}
 
 
 	private Panel createControlPanel() {
+		//PANEL ON THE BOTTOM TO TAKE IN USER INPUT
 	    Panel ctrl =  new Panel(new GridLayout(1,4));
 	   
 	    Button SearchButton=new Button("Search");
 	    TextField SearchText= new TextField();	    		    
-	    Button Marker=new Button("Use Marker");		    
+	    //Button Marker=new Button("Use Marker");		    
 	   // Marker.addActionListener((e)-> { mEmbed.setMarkerBool(true); System.out.println("1");});
 	    
-	    SearchButton.addActionListener((e)-> createInfoPanel(SearchText.getText())); 
+	    //TAKES IN USER INPUT AND SENDS IT TO CREATEINFOPANEL() TO UPDATE THE PANEL
+	    SearchButton.addActionListener((e)-> {
+			try {
+				createInfoPanel(SearchText.getText());
+			} catch (DataNotFoundException e1) {
+				//  Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InvalidFormatException e1) {
+				//  Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}); 
 	    ctrl.add(SearchText);
-	    ctrl.add(SearchButton);
-	    //ctrl.add(mPlayButton);
-	    ctrl.add(Marker);
+	    ctrl.add(SearchButton);	    
+	    //ctrl.add(Marker);
 	   
 	   
 	    return ctrl;
 	}
-	
+	// public static void main(String[] args) throws DataNotFoundException, InvalidFormatException{
+	//	 new MainClass();
+	// }
 	//public boolean getMarkerBool(){
 	//	return MarkerBool;
 	//}
